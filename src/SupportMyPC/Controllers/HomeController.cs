e79ba569e5861a98ccf9051e7e34f232a486ee33
kobiburkis <kobi.burkis@yahoo.com>
2017-07-07 14:01:13 +0300

    Add project files.

---
+++ src/SupportMyPC/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using SupportMyPC.Models;
+using System.Linq;
+
+namespace SupportMyPC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "בוקר טוב" : "צהריים טובים";
+            return View ("MyView");
+        }
+        [HttpGet]
+        public ViewResult SupportForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult SupportSubmit(SupportCall supportResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                supportResponse.SupportCallId = 11111;
+                if (supportResponse.ProblemMachineID == 1)
+                    supportResponse.ProblemMachine = "מחשב נייח";
+                else
+                    supportResponse.ProblemMachine = "מחשב נייד";
+                supportResponse.RegisteredCust = false;
+                supportResponse.SupportCallDate = DateTime.Now.ToString();
+                supportResponse.SupportCallStatusID = 1;
+                supportResponse.SupportCallStatus = "פתוחה";
+                Repository.AddSupportCall(supportResponse);
+                return View("Thanks", supportResponse);
+            }
+            else
+                return View("SupportForm");
+
+
+        }
+        public ViewResult ListSupportCalls()
+        {
+            return View(Repository.SupportCalls.Where(r => r.SupportCallStatusID == 1));
+        }
+    }
+}
---
+++ src/SupportMyPC/Models/SupportCall.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SupportMyPC.Models
+{
+    public class SupportCall
+    {
+        public int SupportCallId { get; set; }
+        [Required(ErrorMessage = "הזן שם לקוח")]
+        public string CustName { get; set; }
+        public string CustEmail { get; set; }
+        [Required(ErrorMessage = "הזן טלפון")]
+        public string CustPhone { get; set; }
+        public bool? RegisteredCust { get; set; }
+        public string ProblemMachine { get; set; }
+        [Required(AllowEmptyStrings =false, ErrorMessage = "לא נבחר ערך")]
+        public int ProblemMachineID { get; set; }
+        public string ProblemDescription { get; set; }
+        public string SupportCallDate { get; set; }
+        public DateTime SupportCallTreatDate { get; set; }
+        public int SupportCallStatusID { get; set; }
+        public string SupportCallStatus { get; set; }
+    }
+}
